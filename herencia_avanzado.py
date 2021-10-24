4b4e489a3f3a421c303c3c346af0aac0b41637f7
sanros <montascito11@gmail.com>
2021-10-24 03:50:07 -0400

    this was pain, this was hard, this was not funny, dude. hard as shit

---
+++ herencia_avanzado.py
@@ -0,0 +1,46 @@
+#
+# ingresar datos por consola, cool
+# limitar la cantidad de valores que introduces por teclado
+# con este bucle solo imprime el numero que el usuario indique
+# [0 for i in range(number)] (es un bucle)
+# 'self' nos ayuda a llamar al metodo
+#
+#
+class calc:
+    def __init__(self, number):
+        self.number = number
+        self.data = [0 for i in range(number)]
+    
+    def set_data(self):
+        self.data = [int(input('set data '+str(i+1)+ '= ')) for i in range(self.number)]
+
+class basic_op(calc):
+    def __init__(self):
+            calc.__init__(self, 2)
+
+    def add(self):
+        a, b, = self.data
+        s = a + b
+        print(s)
+
+    def substract(self):
+        a, b, = self.data
+        r = a - b
+        print(r)
+
+class raiz(calc):
+    def __init__(self):
+            calc.__init__(self, 1)
+    
+    def cuadrada(self):
+        import math
+        a, = self.data
+        print('result', math.sqrt(a))
+
+example = basic_op()
+print(example.set_data())
+print(example.add())
+
+example = raiz()
+print(example.set_data())
+print(example.cuadrada())
